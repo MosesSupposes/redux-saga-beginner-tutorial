8b008fba04d5e9d8659915df279df4e54e56debe
Moses A. Samuel <mosamuel98@gmail.com>
2020-09-08 12:40:53 -0700

    implement my first async saga

--- sagas.js
+++ sagas.js
@@ -1,3 +1,22 @@
+import { put, takeEvery, all } from "redux-saga/effects";
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
 export function* helloSaga() {
 	console.log("Hello Sagas!");
+}
+
+// Our worker Saga: will perform the async increment task
+export function* incrementAsync() {
+	yield delay(1000);
+	yield put({ type: "INCREMENT" });
+}
+
+// Our watcher Saga: spawn a new incremnetAsync task on each INCREMENT_ASYNC
+export function* watchIncrementAsync() {
+	yield takeEvery("INCREMENT_ASYNC", incrementAsync);
+}
+
+export default function* rootSaga() {
+	yield all([helloSaga(), watchIncrementAsync()]);
 }
